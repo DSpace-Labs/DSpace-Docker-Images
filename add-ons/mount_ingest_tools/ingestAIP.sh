4d8441267a6da385d982b354239fe8ba24901a4c
Terry Brady <terrywbrady@gmail.com>
2018-12-18 16:02:15 -0800

    simplify ingest

--- add-ons/mount_ingest_tools/ingestAIP.sh
+++ add-ons/mount_ingest_tools/ingestAIP.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-AIPDIR=${AIPDIR:-/tmp/aip-dir}
+AIPZIP=${AIPZIP:-https://github.com/DSpace-Labs/AIP-Files/raw/master/DogPhotosAIP.zip}
+AIPDIR=/tmp/aip-dir
+rm -rf ${AIPDIR}
+mkdir ${AIPDIR}
 cd ${AIPDIR}
-for file in COMM* COLL* ITEM*;
-do
-  /dspace/bin/dspace packager -r -t AIP -e test@test.edu -f -u $file
-done
+pwd
+curl ${AIPZIP} -L -s --output aip.zip
+unzip aip.zip
+export JAVA_OPTS="${JAVA_OPTS} -Xmx1500m -Dupload.temp.dir=/tmp"
+cd ${AIPDIR}
+/dspace/bin/dspace packager -r -a -t AIP -e test@test.edu -f -u SITE*.zip
+touch /dspace/solr/search/conf/reindex.flag
--- docker-compose-files/dspace-compose/test.sh
+++ docker-compose-files/dspace-compose/test.sh
@@ -1,3 +1,10 @@
 # winpty docker run --rm -it --network ${DPROJ}_dspacenet -v ${DPROJ}_assetstore://dspace/assetstore -v ${DPROJ}_solr://dspace/solr -v /${PWD}/../../add-ons/mount_ingest_tools://ingest-tools -e JAVA_OPTS="-Ddspace.hostname=dspace -Ddspace.baseUrl=http://dspace:8080 -Dsolr.server=http://dspace:8080/solr" dspace/dspace:pr2275 //bin/bash
 
-winpty docker run --rm -it --network ${DPROJ}_dspacenet --volumes-from dspace -e JAVA_OPTS="-Ddspace.hostname=dspace -Ddspace.baseUrl=http://dspace:8080 -Dsolr.server=http://dspace:8080/solr" dspace/dspace:${DSPACE_VER} //dspace/bin/dspace index-discovery
+winpty docker exec -it dspace //ingest-tools/createAdmin.sh
+
+winpty docker exec -it dspace //ingest-tools/ingestAIP.sh
+# winpty docker run --rm -it --network ${DPROJ}_dspacenet --volumes-from dspace -e JAVA_OPTS="-Ddspace.hostname=dspace -Ddspace.baseUrl=http://dspace:8080 -Dsolr.server=http://dspace:8080/solr" dspace/dspace:${DSPACE_VER} //ingest-tools/ingestAIP.sh
+
+sleep 5
+
+winpty docker exec -it dspacedb psql -U dspace -f //ingest-tools/updateSequences.sql
